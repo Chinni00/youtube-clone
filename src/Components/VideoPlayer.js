e8d58a518f1d3bf1a0be63453c9994bdfa4cd2b3
chinni00 <chinni0437@gmail>
2023-07-16 03:12:37 +0530

    sutoplay added

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -10,7 +10,7 @@ const VideoPlayer = () => {
 
     return (
         <div className='m-4 mt-5 border p-3 shadow'>
-            <iframe className='rounded' controls width="1150" height="480" src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}`} title="ISRO&#39;s AMAZING STORY &amp; Chandrayaan | Fuelling India&#39;s Scientific Temper | Akash Banerjee &amp; Avishrant" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe className='rounded' controls width="1150" height="480" src={`https://www.youtube.com/embed/${selectedVideo.id.videoId}?autoplay=1`} title="ISRO&#39;s AMAZING STORY &amp; Chandrayaan | Fuelling India&#39;s Scientific Temper | Akash Banerjee &amp; Avishrant" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             <h2>{selectedVideo.snippet.title}</h2>
             <p className='fw-semibold'>{selectedVideo.snippet.description}</p>
         </div>
